691ced4db84db2bc5d53c8d8dab339836db4dd02
Eetu Knutars <knuuttisr@gmail.com>
2024-02-13 17:45:43 +0200

    Update items.js

--- items.js
+++ items.js
@@ -2,8 +2,8 @@ let tasks = [
 
     // W1
     ["Engineering Apprenticeship", "Collect both keys in Into the Machine and Fire Down Below in World 1."],
-    ["Just Trying to Keep You Alive, Partner", "Watch all 4 binocucom cutscenes in A Stealthy Approach in World 1."],
-    [" Moolah Museum", "Collect at least 100 coins in High Class Heist in World 1, then collect the key."],
+    ["", ""],
+    ["Moolah Museum", "Collect at least 100 coins in High Class Heist in World 1, then collect the key."],
     ["My Lucky Day", "Collect the charm in Prowling the Grounds that triggers Bentley’s dialogue explaining lucky charms and listen to it from start to finish."],
     ["Flat Earth Society", "Destroy all 9 dart guns at least once each disguised as globes in A Cunning Disguise in World 1."],
     ["Sprinting Through the Storm", "Complete any World 1 MTS (Master Thief Sprint)."],
@@ -11,22 +11,22 @@ let tasks = [
     ["Rave Time","Collect the key in the crabs minigame, Treasure in the Depths, in World 1."],
     ["Where We Droppin'?","Hear Raleigh say “fortnite” (or other language equivalent) in The Gunboat Graveyard in World 1. Listen to the full dialogue from start to finish."],
     ["Shh, This is a Library!","Take the barrel disguise all the way to the key in A Cunning Disguise in World 1."],
+    ["Belly Bump Jump (BBJ) in High Class Heist", ""],
 
     // W2
     ["All Bark, No Bite", "Collect the key and vault in any World 2 level that features guards with guns (aka Vinnie the Pincher) without killing any guards, excluding Murray\’s Big Gamble. (Viable levels: Boneyard Casino, Back Alley Heist)"],
     ["Look Out Below!", "Drop all 5 cars suspended over the edge of the rooftops, the radio tower, and the wrecking ball at least once each in Straight to the Top in World 2."],
     ["Cement Bunny Slippers", "Hear Muggshot call you a “jerk” (or other language equivalent) in Straight to the Top in World 2. Listen to the full dialogue from start to finish."],
-    ["A Walk Down Thug Alley", "Protect Murray and collect the key in Murray’s Big Gamble in World 2."],
     ["Environmentally Unfriendly", "Destroy all 6 neon signs portraying Muggshot’s face and all 5 air conditioning units at least once each in Back Alley Heist in World 2."],
     ["Sprinting Through Utah","Complete any World 2 MTS (Master Thief Sprint)."],
     ["Down, Boy","Defeat all dog enemies in any level in World 2."],
     ["Payday!","Collect the coins from the rotating doghouse in the Boneyard Casino in World 2."],
     ["POGDOGTRACK","Collect the key in At the Dog Track in World 2."],
     ["Hot Date","Watch the “date” cutscene with Carmelita before the chase sequence begins in Two to Tango in World 2."],
+    ["Cleetus (any variation) in A Rocky Start", ""],
 
     // W3
     ["Drive-By", "Collect at least 100 coins in A Ghastly Voyage in World 3, then collect the key."],
-    ["Voodoo Simon Says", "Allow Mz. Ruby to explain her upcoming rhythm-based battle phases in A Deadly Dance in World 3. Listen to the full dialogue from start to finish."],
     ["Spooky Scary Skeletons", "Defeat all skeleton type enemies (aka Pierre Flamenose) at least once each in any applicable level in World 3, then collect the key. There are 6 in A Ghastly Voyage and 2 in Descent Into Danger."],
     ["Grave Defiler", "Destroy all 9 ghost generators at least once each in A Grave Undertaking in World 3."],
     ["Zombie Servitude", "Hear Mz. Ruby call out Sly and threaten him with eternal “zombie servitude” (or other language equivalent) in Descent Into Danger in World 3. Listen to the full dialogue from start to finish."],
@@ -35,9 +35,9 @@ let tasks = [
     ["Indiana Cooper","Escape the giant snake in Lair of the Beast in World 3 by following the casual route. This is considered complete at the end of the chase sequence once the cutscene for the snake leaving has finished."],
     ["Raccoon Gumbo","Allow the bomb-toting roosters to defeat you after collecting 20 or more chickens in Down Home Cooking in World 3, then collect the key. You must take a full death to the roosters, exhausting all charms and then dying properly."],
     ["Fish Fry","Restart the fish minigame, Piranha Lake in World 3, after lighting 15 or more torches, then collect the key."],
+    ["DSJ in Dread Swamp Path", ""],
 
     // W4
-    ["Nobody Saw That", "Defeat all 11 guards at least once each in The Unseen Foe in World 4, then collect the key."],
     ["Smells Like Gunpowder", "Set off all gray and red fireworks in any applicable level in World 4 at least once each. There are 14 in A Perilous Ascent, 6 in The Unseen Foe, 16 in Duel by the Dragon, and 1 in Rapid Fire Assault (this does not include firework elevators)."],
     ["POGICETRACK", "Collect the key in A Desperate Race in World 4."],
     ["Cold Date", "Watch the cutscene with Carmelita before the chase sequence begins in Duel by the Dragon in World 4."],
@@ -47,11 +47,11 @@ let tasks = [
     ["No More Kung-Fu!","Defeat all the monkey enemies training on balance poles at least once each in Flaming Temple of Flame in World 4."],
     ["Like a Dragon","Collect the key in Duel by the Dragon in World 4."],
     ["I Got You Covered","Protect Murray and collect the key in The King of the Hill in World 4."],
+    ["Backwards Gate Jump in A Perilous Ascent", ""],
 
     // W5
     ["Smashing Through, Again", "Reach the top platform of Sinking Peril in World 5 that has the jet pack, then reload and do it again. (If you accidentally enter the final level A Strange Reunion, you may view map or reload to Sinking Peril and only do it once more as usual)."],
     ["Together at Last!", "Watch the Carmelita cutscene in A Temporary Truce in World 5. This is considered completed by watching the entire cutscene."],
-    ["Do Flowers Grow in Volcanoes?", "Hear Clockwerk say 3 lines of dialogue while he is malfunctioning in the final platforming section of A Strange Reunion in World 5."],
     ["Put a Lid on It", "Destroy all 3 dart guns at least once each in A Daring Rescue in World 5."],
     ["Calling Air Support", "Let a robo-falcon kill you in A Temporary Truce in World 5."],
     ["Cloudy with a Chance of Meatballs","Let the falling rocks kill you in A Hazardous Path in World 5."],
@@ -59,6 +59,7 @@ let tasks = [
     ["The Ultimate Disguise","Reach Carmelita\’s prison chamber with the protection of a barrel in A Daring Rescue in World 5."],
     ["Haven't Had to Do This in a While","Complete the hacking minigame in Bentley Comes Through in World 5. This requires fully completing the level all the way through to the end, including the final monologue cutscene."],
     ["Owl Historian","Watch the opening cutscene with Bentley\’s jetpack training and Clockwerk\’s dialogue at the beginning of A Strange Reunion."],
+    ["Clockwerk Jump in A Strange Reunion",""],
 
     // General
     ["Extra, Extra!", "Watch at least one of the “newspaper” cutscenes after defeating a world boss. This is considered complete upon watching the full cutscene either after defeating the boss or in the movie menu in the hideout."],
@@ -74,8 +75,8 @@ let tasks = [
     ["1-UP!","Gain an extra life by exceeding the 99-coin maximum while holding a golden lucky charm."],
     ["The Luck of the Irish","Collect all free-standing lucky charms in any level that contains at least two, then use them all before collecting the key at the end of the level. (Viable levels: Two to Tango, Grave Undertaking)."],
     ["Shortcut!","Unlock the gate blocking the second half of any world by collecting three keys and opening the gate. (World 1: Unlock and destroy the generator. World 2: Unlock the car and destroy the boards blocking access to the casino. World 3: Unlock the bone gate with the help of a giant snake. World 4: Unlock the roof hatch with fireworks.)"],
-    ["Going UP","Access the boss level of any world using the intended method. This requires collecting all 7 keys in any world then using the intended method to access the final boss fight. (World 1: Use the cannon to go to The Eye of the Storm. World 2: Use the Muggshot elevator to go to Last Call. World 3: Use the high-pressure soup lid to go to A Deadly Dance. World 4: Use the fireworks to go to Flame-Fu!)."],
-    ["Navigate Like Drake","Enter all levels in any two Worlds (minimum) and show off the fully revealed World maps."]
+    ["Navigate Like Drake","Enter all levels in any two Worlds (minimum) and show off the fully revealed World maps."],
+    ["All Keys in Minimum 2 Worlds", ""]
 ];
 
 let bosses  =  [
